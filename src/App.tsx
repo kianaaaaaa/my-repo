2a2e37b9c47c2d7ab37b1b70f1d971cfa37fc6e4
gongchenzhen <504473065@qq.com>
2022-10-11 15:01:14 +0800

    fff

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
-      <div>为什么会报错???</div>
+      <div>为什么会报错???   </div>
     </div>
   )
 }
